6f80ae7f0d9dd4b2b45031efd50c11e19ac31604
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 22:57:04 +0300

    Добавьте файлы проекта.

---
+++ func7_komar/func7_komar.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void textTime(int m, int h);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int hour, min;
+	cout << "Введите поочередно часы и минуты: " << endl;
+	cin >> min >> hour;
+	if (hour > 24 || min > 60)
+	{
+		cout << "Введите допустимое время!";
+	}
+	else
+	{
+		textTime(hour, min);
+	}
+	return 0;
+}
+
+void textTime(int m, int h)
+{
+	cout << "Сток часов: " << h << endl;
+	cout << "Сток минут: " << m << endl;
+	cout << "Время " << h << ":" << m;
+}
